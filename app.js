e26781e68b73ba46f8c074fd507cf230e5512de1
Susan Ayuque <susanayuque27@gmail.com>
2025-03-01 20:38:05 +1100

    función agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = [];
+let listaDeAmigos = [];
+function agregarAmigo() {
+    let nombres = document.getElementById('amigo').value;
+    console.log(nombres);
+    if (nombres==='') {
+       alert('Ingrese un nombre válido');
+    } else {
+        limpiarCaja();
+        listaDeAmigos.push(nombres);
+        console.log(listaDeAmigos);
+    return;
+    }
+    
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value='';
+}
+
