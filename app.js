56009ce5bf7260b5b44270d223d632d7d3e89fdb
Susanayuque <susanayuque27@gmail.com>
2025-03-05 21:46:49 +1100

    agregar funcion actuliazar amigos

--- app.js
+++ app.js
@@ -4,17 +4,28 @@ function agregarAmigo() {
     let nombres = document.getElementById('amigo').value;
     console.log(nombres);
     if (nombres==='') {
-       alert('Ingrese un nombre válido');
+       alert('Ingrese un nombre válido por favor');
     } else {
         limpiarCaja();
         listaDeAmigos.push(nombres);
         console.log(listaDeAmigos);
+        actualizarListaAmigos('listaAmigos',listaDeAmigos);
     return;
     }
-    
 }
 
 function limpiarCaja(){
     document.querySelector('#amigo').value='';
 }
 
+function actualizarListaAmigos(lista, elementos) {
+    let mostrarListaAmigos = document.getElementById(lista);
+    mostrarListaAmigos.innerHTML = "";
+
+    for (let i = 0; i < elementos.length; i++) {
+        let nuevoNombre = document.createElement('li');
+        nuevoNombre.textContent = elementos[i];
+        mostrarListaAmigos.appendChild(nuevoNombre);
+        
+    }
+} 
